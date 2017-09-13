411898d781dd4b70e4ecbd0e200d4c5ad483370d
funkrider <ian_davies@hotmail.com>
2017-09-13 10:41:15 -0400

    Add various other methods for creating permutations.

--- src/interview_questions/16_Zero_sum_set.clj
+++ src/interview_questions/16_Zero_sum_set.clj
@@ -1,4 +1,5 @@
-(ns interview-questions.16-Zero-sum-set)
+(ns interview-questions.16-Zero-sum-set
+  (:require [clojure.math.combinatorics :as combo]))
 
 ;;; Write a function that takes a list of integers and returns
 ;;; a sequence of all possible groups of 3 integers from the
@@ -6,9 +7,12 @@
 ;;; E.g. (-1, 0, 1, 2, -1, -4) ->
 ;;; ( (-1, 0, 1),  (-1, -1, 2) )
 
-;;; sort -> (-4, -1, -1, 0, 1, 2)
+;;; Note: could have used combinators library to produce combos
+;;; but chose to do by hand.
 
 (defn three-sum [xs]
+  "Takes a sequence of numbers, and returns a list of
+  any group of 3 that sum to zero."
   (let [c (count xs)]
     (->>
       (for   ;; iterate every permutation of items from a sequence
@@ -21,4 +25,33 @@
       (distinct)                        ;; return unique sets
       )
     )
+  )
+
+
+
+
+;;; Write a function that takes a list of integers and returns
+;;; a sequence of all possible groups of 2 or more from the input
+;;; that sum to 0.
+
+;;; Using combinatorial library - first get all possible subsets
+;;; => (combo/subsets [1 2 3])
+;;; (() (1) (2) (3) (1 2) (1 3) (2 3) (1 2 3))
+;;; Then filter to find sum 0!
+
+(defn zero-sum-subsets [xs]
+  (->> xs
+    (combo/subsets)
+    (filter #(and
+               (seq %)
+               (= 0 (apply + %)))))
+  )
+
+(defn set-permutations [xs]
+  (if (= 1 (count xs))
+    (list xs)
+    (for [x xs
+          y (set-permutations (seq (disj (set xs) x)))]
+      (cons x y)
+      ))
   )
